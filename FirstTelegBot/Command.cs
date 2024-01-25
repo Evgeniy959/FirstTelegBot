77f6588f87b0dc2769def1e7d1c4d88919ff55fd
Pili_zr36 <77540303+Evgeniy959@users.noreply.github.com>
2024-01-26 03:01:39 +0700

    Add project files.

---
+++ FirstTelegBot/Command.cs
@@ -0,0 +1,22 @@
+﻿using PRTelegramBot.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FirstTelegBot
+{
+    public class Command
+    {
+        [ReplyMenuHandler("Test", "Hello", "good")]
+        public static async Task Example(ITelegramBotClient botClient, Update update)
+        {
+            var message = "Hello world";
+            //var sendMessage = await PRTelegramBot.Helpers.Message.Send(botClient, update, message);
+            await PRTelegramBot.Helpers.Message.Send(botClient, update, message);
+        }
+    }
+}
---
+++ FirstTelegBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using PRTelegramBot.Core;
+
+const string EXIST_COMMAND = "exist";
+var telegram = new PRBot(options =>
+{
+    options.Token = "6533200832:AAFwWxynCNrtd6j846rsWINlc8Q26Q3_hnQ";
+    options.ClearUpdatesOnStart = true;
+    options.WhiteListUsers = new List<long>() { };
+    options.Admins = new List<long>() { };
+    options.BotId = 0;
+});
+telegram.OnLogCommon += Telegram_OnLogCommon;
+telegram.OnLogError += Telegram_OnLogError;
+await telegram.Start();
+void Telegram_OnLogCommon(string msg, Enum typeEvent, ConsoleColor color) 
+{
+    //Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.ForegroundColor = color;
+    Console.WriteLine($"{DateTime.Now}:{msg}");
+    Console.ResetColor();
+}
+void Telegram_OnLogError(Exception ex, long? id)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{DateTime.Now}:{ex}");
+    Console.ResetColor();
+}
+
+while (true)
+{
+    var result = Console.ReadLine();
+    if (result.ToLower() == EXIST_COMMAND)
+    {
+        Environment.Exit(0);
+    }
+}
